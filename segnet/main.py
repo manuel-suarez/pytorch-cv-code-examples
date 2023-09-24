9606aa4612dd20f5259a8c8c6ba2c3c32ed6f0a4
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-24 10:26:07 -0600

    Initial setup

---
+++ segnet/main.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+import os
+from os import path
+import torchvision
+import torchvision.transforms as T
+from typing import Sequence
+from torchvision.transforms import functional as F
+import numbers
+import random
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import torchmetrics as TM
+
+# Convert a pytorch tensor into a PIL image
+t2img = T.ToPilImage()
+# Convert a PIL image into a pytorch tensor
+img2t = T.ToTensor()
