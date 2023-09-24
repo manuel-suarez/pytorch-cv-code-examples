1b16d614e9abde33f48f57a69341a348eed0c88c
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-24 12:05:27 -0600

    Definition of auxiliary functions

--- segnet/main.py
+++ segnet/main.py
@@ -17,3 +17,50 @@ import torchmetrics as TM
 t2img = T.ToPILImage()
 # Convert a PIL image into a pytorch tensor
 img2t = T.ToTensor()
+
+# Set the working (writable) directory
+working_dir = "/home/mario.canul/data/segnet"
+
+#
+def save_model_checkpoint(model, cp_name):
+    torch.save(model.state_dict(), os.path.join(working_dir, cp_name))
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    else:
+        return torch.device("cpu")
+
+# Load model from saved checkpoint
+def load_model_from_checkpoint(model, ckp_path):
+    return model.load_state_dict(
+        torch.load(
+            ckp_path,
+            map_location=get_device()
+        )
+    )
+
+# Send the Tensor or Model (input argument x) to the right device
+# for this notebook, i.e. if GPU is enabled, then send to GPU/CUDA
+# otherwise send to CPU
+def to_device(x):
+    if torch.cuda.is_available():
+        return x.cuda()
+    else:
+        return x.cpu()
+
+def get_model_parameters(m):
+    total_params = sum(
+        param.numel() for param in m.parameters()
+    )
+    return total_params
+
+def print_model_parameters(m):
+    num_model_parameters = get_model_parameters(m)
+    print(f"The Model has {num_model_parameters/1e6:.2f}M parameters")
+
+def close_figures():
+    while len(plt.get_fignums()) > 0:
+        plt.close()
+
+print("f"CUDA: {torch.cuda.is_available()})
