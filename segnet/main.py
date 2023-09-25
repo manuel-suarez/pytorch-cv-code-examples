db4324c75f136d517ed0baad74a99d73a32cef90
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-24 23:07:07 -0600

    Define model with separable convolutions

--- segnet/main.py
+++ segnet/main.py
@@ -607,6 +607,181 @@ def train_loop(model, loader, test_data, epochs, optimizer, scheduler, save_path
     # end for
 # end def
 
+print_model_parameters(m)
+
+# SegNet Basic with depthwise-separable convolutions
+
+# Model definition. We use a SegNet-Basic model with some minor tweaks.
+# Our input images are 128x128.
+# In this model, we use depth-wise-separable convolutions instead of
+# "regular" convolutions.
+
+class DepthwiseSeparableConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding, bias=True):
+        super().__init__()
+        # The depthwise conv is basically just a grouped convolution in PyTorch with
+        # the number of distinct groups being the same as the number of input (and output)
+        # channels for that layer.
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, padding=padding, bias=bias,
+                                   groups=in_channels)
+        # The pointwise convolution stretches across all the output channels using
+        # a 1x1 kernel.
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=bias)
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
+
+
+class DownDSConv2(nn.Module):
+    def __init__(self, chin, chout, kernel_size):
+        super().__init__()
+        self.seq = nn.Sequential(
+            DepthwiseSeparableConv2d(in_channels=chin, out_channels=chout, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chout),
+            nn.ReLU(),
+            DepthwiseSeparableConv2d(in_channels=chout, out_channels=chout, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chout),
+            nn.ReLU(),
+        )
+        self.mp = nn.MaxPool2d(kernel_size=2, return_indices=True)
+
+    def forward(self, x):
+        y = self.seq(x)
+        pool_shape = y.shape
+        y, indices = self.mp(y)
+        return y, indices, pool_shape
+
+
+class DownDSConv3(nn.Module):
+    def __init__(self, chin, chout, kernel_size):
+        super().__init__()
+        self.seq = nn.Sequential(
+            DepthwiseSeparableConv2d(in_channels=chin, out_channels=chout, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chout),
+            nn.ReLU(),
+            DepthwiseSeparableConv2d(in_channels=chout, out_channels=chout, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chout),
+            nn.ReLU(),
+            DepthwiseSeparableConv2d(in_channels=chout, out_channels=chout, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chout),
+            nn.ReLU(),
+        )
+        self.mp = nn.MaxPool2d(kernel_size=2, return_indices=True)
+
+    def forward(self, x):
+        y = self.seq(x)
+        pool_shape = y.shape
+        y, indices = self.mp(y)
+        return y, indices, pool_shape
+
+
+class UpDSConv2(nn.Module):
+    def __init__(self, chin, chout, kernel_size):
+        super().__init__()
+        self.seq = nn.Sequential(
+            DepthwiseSeparableConv2d(in_channels=chin, out_channels=chin, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chin),
+            nn.ReLU(),
+            DepthwiseSeparableConv2d(in_channels=chin, out_channels=chout, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chout),
+            nn.ReLU(),
+        )
+        self.mup = nn.MaxUnpool2d(kernel_size=2)
+
+    def forward(self, x, indices, output_size):
+        y = self.mup(x, indices, output_size=output_size)
+        y = self.seq(y)
+        return y
+
+
+class UpDSConv3(nn.Module):
+    def __init__(self, chin, chout, kernel_size):
+        super().__init__()
+        self.seq = nn.Sequential(
+            DepthwiseSeparableConv2d(in_channels=chin, out_channels=chin, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chin),
+            nn.ReLU(),
+            DepthwiseSeparableConv2d(in_channels=chin, out_channels=chin, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chin),
+            nn.ReLU(),
+            DepthwiseSeparableConv2d(in_channels=chin, out_channels=chout, kernel_size=kernel_size,
+                                     padding=kernel_size // 2, bias=False),
+            nn.BatchNorm2d(chout),
+            nn.ReLU(),
+        )
+        self.mup = nn.MaxUnpool2d(kernel_size=2)
+
+    def forward(self, x, indices, output_size):
+        y = self.mup(x, indices, output_size=output_size)
+        y = self.seq(y)
+        return y
+
+
+class ImageSegmentationDSC(torch.nn.Module):
+    def __init__(self, kernel_size):
+        super().__init__()
+        self.out_channels = 3
+        self.bn_input = nn.BatchNorm2d(3)
+        self.dc1 = DownDSConv2(3, 64, kernel_size=kernel_size)
+        self.dc2 = DownDSConv2(64, 128, kernel_size=kernel_size)
+        self.dc3 = DownDSConv3(128, 256, kernel_size=kernel_size)
+        self.dc4 = DownDSConv3(256, 512, kernel_size=kernel_size)
+        # self.dc5 = DownConv3(512, 512, kernel_size=kernel_size)
+
+        # self.uc5 = UpConv3(512, 512, kernel_size=kernel_size)
+        self.uc4 = UpDSConv3(512, 256, kernel_size=kernel_size)
+        self.uc3 = UpDSConv3(256, 128, kernel_size=kernel_size)
+        self.uc2 = UpDSConv2(128, 64, kernel_size=kernel_size)
+        self.uc1 = UpDSConv2(64, 3, kernel_size=kernel_size)
+
+    def forward(self, batch: torch.Tensor):
+        x = self.bn_input(batch)
+        # x = batch
+        # SegNet Encoder
+        x, mp1_indices, shape1 = self.dc1(x)
+        x, mp2_indices, shape2 = self.dc2(x)
+        x, mp3_indices, shape3 = self.dc3(x)
+        x, mp4_indices, shape4 = self.dc4(x)
+        # Our images are 128x128 in dimension. If we run 4 max pooling
+        # operations, we are down to 128/16 = 8x8 activations. If we
+        # do another down convolution, we'll be at 4x4 and at that point
+        # in time, we may lose too much spatial information as a result
+        # of the MaxPooling operation, so we stop at 4 down conv
+        # operations.
+        # x, mp5_indices, shape5 = self.dc5(x)
+
+        # SegNet Decoder
+        # x = self.uc5(x, mp5_indices, output_size=shape5)
+        x = self.uc4(x, mp4_indices, output_size=shape4)
+        x = self.uc3(x, mp3_indices, output_size=shape3)
+        x = self.uc2(x, mp2_indices, output_size=shape2)
+        x = self.uc1(x, mp1_indices, output_size=shape1)
+
+        return x
+    # end def
+# end class
+
+# Run the model once on a single input batch to make sure that the model
+# runs as expected and returns a tensor with the expected shape.
+mdsc = ImageSegmentationDSC(kernel_size=3)
+mdsc.eval()
+to_device(mdsc)
+mdsc(to_device(train_pets_inputs)).shape
+
+print_model_parameters(mdsc)
+
+
 # Train our model for 20 epochs, and record the following:
 #
 # 1. Training Loss
@@ -627,9 +802,8 @@ def train_loop(model, loader, test_data, epochs, optimizer, scheduler, save_path
 # epoch.
 #
 save_path = os.path.join(working_dir, "segnet_basic_training_progress_images")
-train_loop(m, pets_train_loader, (test_pets_inputs, test_pets_targets), (1, 101), optimizer, scheduler, save_path)
+train_loop(m, pets_train_loader, (test_pets_inputs, test_pets_targets), (1, 5), optimizer, scheduler, save_path)
 
 # Save the model's checkpoint.
 save_model_checkpoint(m, f"pets_segnet_CrossEntropyLoss_LRSchedule_20_epochs.pth")
 
-print_model_parameters(m)
