426ce1b4ea9dc2a127883b5fd587367e1283fa02
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-24 20:49:08 -0600

    Eval computational cost

--- segnet/eval_parameters.py
+++ segnet/eval_parameters.py
@@ -6,3 +6,8 @@ dk, m, n = 3, 16, 32
 print(f"Expected number of parameters: {m * dk * dk * n}")
 conv1 = nn.Conv2d(in_channels=m, out_channels=n, kernel_size=dk, bias=False)
 print(f"Actual number of parameters: {num_parameters(conv1)}")
+
+from torchinfo import summary
+h, w = 128, 128
+print(f"Expected total multiplies: {m * dk * dk * h * w * n}")
+summary(conv1, input_size=(1, m, h, w))
