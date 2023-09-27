f9d1b91bc6a043be75e40625bca27e32fb9ebec4
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-27 01:01:21 -0600

    Model definition

--- deepglobe-land-cover-classification/main.py
+++ deepglobe-land-cover-classification/main.py
@@ -243,3 +243,17 @@ for idx in range(3):
         ),
         one_hot_encoded_mask = reverse_one_hot(mask)
     )
+
+ENCODER = 'resnet50'
+ENCODER_WEIGHTS = 'imagenet'
+CLASSES = select_classes
+ACTIVATION = 'sigmoid' # could be None for logits or 'softmax2d' for multiclass segmentation
+
+# create segmentation model with pretrained encoder
+model = smp.DeepLabV3Plus(
+    encoder_name=ENCODER,
+    encoder_weights=ENCODER_WEIGHTS,
+    classes=len(CLASSES),
+    activation=ACTIVATION
+)
+preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
