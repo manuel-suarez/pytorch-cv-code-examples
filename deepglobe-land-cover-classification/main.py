a1022278f26a5766dc79fc520508a4adfaf9a9a1
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-26 20:28:59 -0600

    Load environment variables and configure path dataset

--- deepglobe-land-cover-classification/main.py
+++ deepglobe-land-cover-classification/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import random, tqdm
 import seaborn as sns
 import matplotlib.pyplot as plt
+from dotenv import load_dotenv
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -15,3 +16,21 @@ import albumentations as album
 
 import segmentation_models_pytorch as smp
 
+# Load environment variables
+load_dotenv()
+
+DATA_DIR = os.getenv('DATA_DIR')
+
+metadata_df = pd.read_csv(os.path.join(DATA_DIR, 'metadata.csv'))
+metadata_df = metadata_df[metadata_df['split']=='train']
+metadata_df = metadata_df[['image_id', 'sat_image_path', 'mask_path']]
+metadata_df['sat_image_path'] = metadata_df['sat_image_path'].apply(
+    lambda img_pth: os.path.join(DATA_DIR, img_pth))
+metadata_df['mask_path'] = metadata_df['mask_path'].apply(
+    lambda img_pth: os.path.join(DATA_DIR, img_pth))
+metadata_df = metadata_df.sample(frac=1).reset_index(drop=True)
+
+# Perform 90/10 split for train/val
+valid_df = metadata_df.sample(frac=0.1, random_state=42)
+train_df = metadata_df.drop(valid_df.index)
+len(train_df), len(valid_df)
