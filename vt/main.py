f64aa3518b6d82c0ab4d9204ecf0eca2321c7be6
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-29 16:45:13 -0600

    Training test and saving figures

--- vt/main.py
+++ vt/main.py
@@ -500,3 +500,149 @@ def test_custom_iou_loss():
 
 print_title("Test custom IoU loss")
 print(test_custom_iou_loss())
+
+# Train the model for a single epoch
+def train_model(model, loader, optimizer):
+    to_device(model.train())
+    cel = True
+    if cel:
+        criterion = nn.CrossEntropyLoss(reduction='mean')
+    else:
+        criterion = IoULoss(softmax=True)
+
+    running_loss = 0.0
+    running_samples = 0
+
+    for batch_idx, (inputs, targets) in enumerate(loader, 0):
+        optimizer.zero_grad()
+        inputs = to_device(inputs)
+        targets = to_device(targets)
+        outputs = model(inputs)
+
+        # The ground truth labels have a channel dimension (NCHW).
+        # We need to remove it before passing it into
+        # CrossEntropyLoss so that it has shape (NHW) and each element
+        # is a value representing the class of the pixel.
+        if cel:
+            targets = targets.squeeze(dim=1)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        running_samples += targets.size(0)
+        running_loss += loss.item()
+
+    print("Trained {} samples, Loss: {:4f}".format(
+        running_samples,
+        running_loss / (batch_idx+1)
+    ))
+
+def prediction_accuracy(ground_truth_labels, predicted_labels):
+    eq = ground_truth_labels == predicted_labels
+    return eq.sum().item() / predicted_labels.numel()
+
+def print_test_dataset_masks(model, test_pets_targets, test_pets_labels, epoch, save_path, show_plot):
+    to_device(model.eval())
+    predictions = model(to_device(test_pets_targets))
+    test_pets_labels = to_device(test_pets_labels)
+    # print("Predictions Shape: {}".format(predictions.shape))
+    pred = nn.Softmax(dim=1)(predictions)
+
+    pred_labels = pred.argmax(dim=1)
+    # Add a value 1 dimension at dim=1
+    pred_labels = pred_labels.unsqueeze(1)
+    # print("pred_labels.shape: {}".format(pred_labels.shape))
+    pred_mask = pred_labels.to(torch.float)
+
+    # accuracy = prediction_accuracy(test_pets_labels, pred_labels)
+    iou = to_device(TM.classification.MulticlassJaccardIndex(3, average='micro', ignore_index=TrimapClasses.BACKGROUND))
+    iou_accuracy = iou(pred_mask, test_pets_labels)
+    pixel_metric = to_device(TM.classification.MulticlassAccuracy(3, average='micro'))
+    pixel_accuracy = pixel_metric(pred_labels, test_pets_labels)
+    custom_iou = IoUMetric(pred, test_pets_labels)
+    title = f'Epoch: {epoch:02d}, Accuracy[Pixel: {pixel_accuracy:.4f}, IoU: {iou_accuracy:.4f}, Custom IoU: {custom_iou:.4f}]'
+    print(title)
+    # print(f"Accuracy: {accuracy:.4f}")
+
+    # Close all previously open figures.
+    close_figures()
+
+    fig = plt.figure(figsize=(10, 12))
+    fig.suptitle(title, fontsize=12)
+
+    fig.add_subplot(3, 1, 1)
+    plt.imshow(t2img(torchvision.utils.make_grid(test_pets_targets, nrow=7)))
+    plt.axis('off')
+    plt.title("Targets")
+
+    fig.add_subplot(3, 1, 2)
+    plt.imshow(t2img(torchvision.utils.make_grid(test_pets_labels.float() / 2.0, nrow=7)))
+    plt.axis('off')
+    plt.title("Ground Truth Labels")
+
+    fig.add_subplot(3, 1, 3)
+    plt.imshow(t2img(torchvision.utils.make_grid(pred_mask / 2.0, nrow=7)))
+    plt.axis('off')
+    plt.title("Predicted Labels")
+
+    if save_path is not None:
+        plt.savefig(os.path.join(save_path, f"epoch_{epoch:02}.png"), format="png", bbox_inches="tight", pad_inches=0.4)
+    # end if
+
+    if show_plot is False:
+        close_figures()
+    else:
+        plt.show()
+    # end if
+# end def
+
+def test_dataset_accuracy(model, loader):
+    to_device(model.eval())
+    iou = to_device(TM.classification.MulticlassJaccardIndex(3, average='micro', ignore_index=TrimapClasses.BACKGROUND))
+    pixel_metric = to_device(TM.classification.MulticlassAccuracy(3, average='micro'))
+
+    iou_accuracies = []
+    pixel_accuracies = []
+    custom_iou_accuracies = []
+
+    print_model_parameters(model)
+
+    for batch_idx, (inputs, targets) in enumerate(loader, 0):
+        inputs = to_device(inputs)
+        targets = to_device(targets)
+        predictions = model(inputs)
+
+        pred_probabilities = nn.Softmax(dim=1)(predictions)
+        pred_labels = predictions.argmax(dim=1)
+
+        # Add a value 1 dimension at dim=1
+        pred_labels = pred_labels.unsqueeze(1)
+        # print("pred_labels.shape: {}".format(pred_labels.shape))
+        pred_mask = pred_labels.to(torch.float)
+
+        iou_accuracy = iou(pred_mask, targets)
+        # pixel_accuracy = pixel_metric(pred_mask, targets)
+        pixel_accuracy = pixel_metric(pred_labels, targets)
+        custom_iou = IoUMetric(pred_probabilities, targets)
+        iou_accuracies.append(iou_accuracy.item())
+        pixel_accuracies.append(pixel_accuracy.item())
+        custom_iou_accuracies.append(custom_iou.item())
+
+        del inputs
+        del targets
+        del predictions
+    # end for
+
+    iou_tensor = torch.FloatTensor(iou_accuracies)
+    pixel_tensor = torch.FloatTensor(pixel_accuracies)
+    custom_iou_tensor = torch.FloatTensor(custom_iou_accuracies)
+
+    print("Test Dataset Accuracy")
+    print(
+        f"Pixel Accuracy: {pixel_tensor.mean():.4f}, IoU Accuracy: {iou_tensor.mean():.4f}, Custom IoU Accuracy: {custom_iou_tensor.mean():.4f}")
+
+m = vit
+images_folder_name = "vit_training_progress_images"
+save_path = os.path.join(working_dir, images_folder_name)
+os.makedirs(save_path, exist_ok=True)
+print_test_dataset_masks(m, test_pets_inputs, test_pets_targets, epoch=0, save_path=None, show_plot=True)
