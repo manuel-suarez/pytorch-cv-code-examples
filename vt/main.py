b829e9de2cf57db0f53a690f71fd632fc29b2c94
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-28 23:39:09 -0600

    Define auxiliary functions

--- vt/main.py
+++ vt/main.py
@@ -26,3 +26,68 @@ load_dotenv()
 
 # Set the working (writable) directory.
 working_dir = os.getenv('WORKING_DIR')
+
+
+def save_model_checkpoint(model, cp_name):
+    torch.save(model.state_dict(), os.path.join(working_dir, cp_name))
+
+
+def get_device():
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    else:
+        return torch.device("cpu")
+
+
+# Load model from saved checkpoint
+def load_model_from_checkpoint(model, ckp_path):
+    return model.load_state_dict(
+        torch.load(
+            ckp_path,
+            map_location=get_device(),
+        )
+    )
+
+
+# Send the Tensor or Model (input argument x) to the right device
+# for this notebook. i.e. if GPU is enabled, then send to GPU/CUDA
+# otherwise send to CPU.
+def to_device(x):
+    if torch.cuda.is_available():
+        return x.cuda()
+    else:
+        return x.cpu()
+
+
+def get_model_parameters(m):
+    total_params = sum(
+        param.numel() for param in m.parameters()
+    )
+    return total_params
+
+
+def print_model_parameters(m):
+    num_model_parameters = get_model_parameters(m)
+    print(f"The Model has {num_model_parameters / 1e6:.2f}M parameters")
+
+
+# end if
+
+def close_figures():
+    while len(plt.get_fignums()) > 0:
+        plt.close()
+    # end while
+
+
+# end def
+
+def print_title(title):
+    title_len = len(title)
+    dashes = ''.join(["-"] * title_len)
+    print(f"\n{title}\n{dashes}")
+
+
+# end def
+
+# Validation: Check if CUDA is available
+print(f"CUDA: {torch.cuda.is_available()}")
