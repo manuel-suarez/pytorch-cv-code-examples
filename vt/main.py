e9f609bc71b7201fc2f17e16bfe608dbe544d66e
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-28 23:38:31 -0600

    Configure dotfiles

--- vt/main.py
+++ vt/main.py
@@ -14,3 +14,15 @@ from matplotlib import pyplot as plt
 import torchmetrics as TM
 from dataclasses import dataclass
 import dataclasses
+from dotenv import load_dotenv
+
+# Convert a pytorch tensor into a PIL image
+t2img = T.ToPILImage()
+# Convert a PIL image into a pytorch tensor
+img2t = T.ToTensor()
+
+# Load environment variables
+load_dotenv()
+
+# Set the working (writable) directory.
+working_dir = os.getenv('WORKING_DIR')
