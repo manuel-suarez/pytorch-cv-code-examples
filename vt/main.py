fa6a46c93fc8d290641a63137fa47240782d4c93
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-28 23:35:44 -0600

    Initial configuration

---
+++ vt/main.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+import os
+from os import path
+import torchvision
+import torchvision.transforms as T
+from typing import Sequence
+from torchvision.transforms import functional as F
+import numbers
+import random
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import torchmetrics as TM
+from dataclasses import dataclass
+import dataclasses
