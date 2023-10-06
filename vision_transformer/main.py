48cae80ca3dc8a4f79de1b7fa23cfeb9da057ca6
Arturo Suárez <manuel.suarez@cimat.mx>
2023-10-06 17:02:10 -0600

    Transpose patches of image preserving channels

--- vision_transformer/main.py
+++ vision_transformer/main.py
@@ -32,12 +32,22 @@ plt.savefig('figure02.png')
 plt.close(fig)
 
 patch_size = 16 # 16 pixels
+# Figure patches
+img_patches = rearrange(x, 'b c (h s1) (w s2) -> b c (h w) (s1 s2)', s1=patch_size, s2=patch_size)
+print(img_patches.shape)
+img_patches = np.transpose(img_patches, (1, 2, 0))
+fig = plt.figure()
+plt.imshow(img_patches)
+plt.savefig('figure03.png')
+plt.close(fig)
+
+# Embeeding patches
 patches = rearrange(x, 'b c (h s1) (w s2) -> b (h w) (s1 s2 c)', s1=patch_size, s2=patch_size)
 print(patches.shape)
 patches = np.transpose(patches, (1, 2, 0))
 fit = plt.figure()
 plt.imshow(patches)
-plt.savefig('figure03.png')
+plt.savefig('figure04.png')
 plt.close(fig)
 
 class PatchEmbedding(nn.Module):
@@ -60,5 +70,5 @@ xpatched = xpatched.detach().numpy()
 xpatched = np.transpose(xpatched, (1, 2, 0))
 fig = plt.figure()
 plt.imshow(xpatched)
-plt.savefig('figure04.png')
+plt.savefig('figure05.png')
 plt.close(fig)
