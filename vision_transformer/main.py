ba6f4582dd6714a2819f1d0ed5310d1be5c817f8
Arturo Suárez <manuel.suarez@cimat.mx>
2023-10-06 16:04:33 -0600

    Open and resize

--- vision_transformer/main.py
+++ vision_transformer/main.py
@@ -9,3 +9,16 @@ from torchvision.transforms import Compose, Resize, ToTensor
 from einops import rearrange, reduce, repeat
 from einops.layers.torch import Rearrange, Reduce
 from torchsummary import summary
+
+# Open image
+img = Image.open('./cat.jpg')
+fig = plt.figure()
+plt.imshow(img)
+plt.savefig('figure01.png0')
+plt.close(fig)
+
+# Resize
+transform = Compose([Resize((224, 224)), ToTensor()])
+x = transform(img)
+x = x.unsqueeze(0) # add batch dim
+print(x.shape)
