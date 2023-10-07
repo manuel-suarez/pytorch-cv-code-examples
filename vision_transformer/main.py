29b79ae2f310802f68ae1f05dd4157296e281feb
Arturo Suárez <manuel.suarez@cimat.mx>
2023-10-07 12:20:15 -0600

    Vision Transformer

--- vision_transformer/main.py
+++ vision_transformer/main.py
@@ -178,3 +178,20 @@ class ClassificationHead(nn.Sequential):
             nn.LayerNorm(emb_size),
             nn.Linear(emb_size, n_classes)
         )
+
+class ViT(nn.Sequential):
+    def __init__(self,
+                 in_channels: int = 3,
+                 patch_size: int = 16,
+                 emb_size: int = 768,
+                 img_size: int = 224,
+                 depth: int = 12,
+                 n_classes: int = 1000,
+                 **kwargs):
+        super().__init__(
+            PatchEmbedding(in_channels, patch_size, emb_size, img_size),
+            TransformerEncoder(depth, emb_size=emb_size, **kwargs),
+            ClassificationHead(emb_size, n_classes)
+        )
+
+summary(ViT(), (3, 224, 224), device='cpu')
