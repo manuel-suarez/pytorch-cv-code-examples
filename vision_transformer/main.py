65d50b64b5fd895fb71e64607ae8f19799bde23c
Arturo Suárez <manuel.suarez@cimat.mx>
2023-10-07 10:36:24 -0600

    Add CLS Token (position embedding)

--- vision_transformer/main.py
+++ vision_transformer/main.py
@@ -54,9 +54,14 @@ class PatchEmbedding(nn.Module):
             nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
             Rearrange('b e (h) (w) -> b (h w) e'),
         )
+        self.cls_token = nn.Parameter(torch.randn(1,1, emb_size))
 
     def forward(self, x: Tensor) -> Tensor:
+        b, _, _, _ = x.shape
         x = self.projection(x)
+        cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
+        # prepend the cls token to the input
+        x = torch.cat([cls_tokens, x], dim=1)
         return x
 
 xpatched = PatchEmbedding()(x)
