4601a0c0d3b1467b02f5e3fc1f0520ed8a2944fb
Arturo Suárez <manuel.suarez@cimat.mx>
2023-10-07 11:45:34 -0600

    MLP

--- vision_transformer/main.py
+++ vision_transformer/main.py
@@ -125,3 +125,12 @@ class ResidualAdd(nn.Module):
         x = self.fn(x, **kwargs)
         x += res
         return x
+
+class FeedForwardBlock(nn.Sequential):
+    def __init__(self, emb_size: int, expansion: int = 4, drop_p: float = 0.):
+        super().__init__(
+            nn.Linear(emb_size, expansion * emb_size),
+            nn.GELU(),
+            nn.Dropout(drop_p),
+            nn.Linear(expansion * emb_size, emb_size),
+        )
